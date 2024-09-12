dfef20de3dfd31cf6dfdf93ad39fdb1166b1c82d
SoloAbyss <maximusabrahamse@gmail.com>
2024-09-12 14:26:35 +1200

    Slider half working

--- Testing/script.js
+++ Testing/script.js
@@ -1,5 +1,5 @@
 let slider = document.querySelector('.slider');
-let innerSlider = document.querySelector('slider-inner');
+let innerSlider = document.querySelector('.slider-inner');
 
 let pressed = false;
 let startx;
@@ -8,5 +8,30 @@ let x;
 slider.addEventListener('mousedown', (e)=>{
     pressed = true;
     startx = e.offsetX - innerSlider.offsetLeft;
-    console.log(innerSlider.offsetLeft)
-})
+    slider.style.cursor = 'grabbing'
+});
+
+slider.addEventListener('mouseenter', ()=>{
+    slider.style.cursor = 'grab'
+});
+
+// slider.addEventListener('mouseleave', ()=>{
+//     slider.style.cursor = 'default'
+// })
+
+slider.addEventListener('mouseup', ()=>{
+    slider.style.cursor = 'grab'
+});
+
+window.addEventListener('mosueup', ()=>{
+    pressed = false;
+});
+
+slider.addEventListener('mousemove', (e)=>{
+    if(!pressed) return;
+    e.preventDefault();
+
+    x = e.offsetX
+
+    innerSlider.style.left = `${x - startx}px`
+});
